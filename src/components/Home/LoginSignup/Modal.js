3609a08b5586fdd680e7f7ce594ea5cd0536d16d
Naël Achkar <naelachkar@hotmail.fr>
2022-12-19 14:49:44 +0200

    Modal functionalities done

--- src/components/Home/LoginSignup/Modal.js
+++ src/components/Home/LoginSignup/Modal.js
@@ -5,11 +5,11 @@ export default function Modal({ isModalOpen, toggleModal, loginOrSignup }) {
   return (
     <div>
       {isModalOpen && (
-        <div className="modal">
-          <div className="modal-body">
-            <h1>{`${loginOrSignup}`}</h1>
-            {loginOrSignup === "Log in" ? <LoginForm /> : <SignupForm />}
-            <button onClick={toggleModal}>Close</button>
+        <div className="modal-overlay" onClick={toggleModal}>
+          <div className="modal">
+              <h2>{`${loginOrSignup}`}</h2>
+              {loginOrSignup === "Log in" ? <LoginForm /> : <SignupForm />}
+              <button onClick={toggleModal}>Close</button>
           </div>
         </div>
       )}
