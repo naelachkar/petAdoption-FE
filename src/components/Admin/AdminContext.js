5ed81adb1a6c7bfa528e2ea0be840a4ba551c6ca
Naël Achkar <naelachkar@hotmail.fr>
2023-01-04 15:06:41 +0200

    adming user page functionalities done

--- src/components/Admin/AdminContext.js
+++ src/components/Admin/AdminContext.js
@@ -5,6 +5,7 @@ export const AdminContext = createContext();
 
 export default function AdminContextWrapper({ children }) {
   const [allUsers, setAllUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState();
 
   const getAllUsers = async () => {
     const token = JSON.parse(localStorage.getItem("token"));
@@ -20,8 +21,23 @@ export default function AdminContextWrapper({ children }) {
     }
   };
 
+  const getUserByIdFull = async (id) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    const headersConfig = { headers: { Authorization: `Bearer ${token}` } };
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_URL}/user/:${id}/full`,
+        headersConfig
+      );
+      setSelectedUser(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <AdminContext.Provider value={{ getAllUsers, allUsers }}>
+    <AdminContext.Provider
+      value={{ getAllUsers, allUsers, getUserByIdFull, selectedUser }}>
       {children}
     </AdminContext.Provider>
   );
--- src/components/Admin/UserInfo.js
+++ src/components/Admin/UserInfo.js
@@ -1,10 +1,28 @@
+import { useContext, useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
+import { AdminContext } from "./AdminContext";
 
 export default function UserInfo() {
+  const id = location.search.slice(4);
+
+  const { getUserByIdFull, selectedUser } = useContext(AdminContext);
+
+  useEffect(() => {
+    getUserByIdFull(id);
+  }, []);
+
+  if (!selectedUser) return;
+
   return (
     <>
       <NavBar />
-      <h1>User Information</h1>
+      <h1>{selectedUser.firstName} {selectedUser.lastName}</h1>
+      <ul>
+        <li>Email: {selectedUser.email}</li>
+        <li>Phone number: {selectedUser.phoneNumber}</li>
+        <li>Bio: {selectedUser.bio}</li>
+        <li>Admin: {selectedUser.admin}</li>
+      </ul>
     </>
   );
 }
