9968ecb987d550e666d5a1312d19d894ca7bf536
Naël Achkar <naelachkar@hotmail.fr>
2023-01-09 19:52:53 +0200

    Saving pet functionality done

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -2,11 +2,12 @@ import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import { PetContext } from "./PetContext";
+import "./Pet.css";
 
 export default function Pet() {
   const location = useLocation();
   const currentId = location.search.slice(4);
-  const { getPetById, currentPet } = useContext(PetContext);
+  const { getPetById, currentPet, savePet } = useContext(PetContext);
 
   useEffect(() => {
     getPetById(currentId);
@@ -14,25 +15,55 @@ export default function Pet() {
 
   if (!currentPet) return <NavBar />;
 
-  document.title = currentPet.name;
+  const {
+    name,
+    type,
+    picture,
+    breed,
+    adoptionStatus,
+    color,
+    height,
+    weight,
+    hypoallergenic,
+    bio,
+  } = currentPet;
+
+  document.title = name;
+
+  let actionButton;
+  if (adoptionStatus === "Adopted") {
+    actionButton = <button className="unavailable">Adopted</button>;
+  } else if (adoptionStatus === "Fostered") {
+    actionButton = <button className="unavailable">Fostered</button>;
+  } else if (adoptionStatus === "Available") {
+    actionButton = (
+      <>
+        <button>Adopt</button>
+        <button>Foster</button>
+      </>
+    );
+  }
+
+  const saveButton = <button onClick={() => savePet(currentId)}>Save</button>;
 
   return (
     <>
       <NavBar />
-      <h1>{currentPet.name}</h1>
-      <h2>{currentPet.type}</h2>
-      {currentPet.picture ? <img src={currentPet.picture} /> : null}
-      <h3>{currentPet.breed}</h3>
-      <h4>{currentPet.adoptionStatus}</h4>
+      <h1>{name}</h1>
+      <h2>{type}</h2>
+      {picture ? <img src={picture} /> : null}
+      <h3>{breed}</h3>
+      <div>
+        {actionButton}
+        {saveButton}
+      </div>
       <ul>
-        <li>Colour: {currentPet.color}</li>
-        <li>Height: {currentPet.height}cm</li>
-        <li>Weight: {currentPet.weight}kg</li>
-        <li>Hypoallergenic: {currentPet.hypoallergenic ? "Yes" : "No"}</li>
+        <li>Colour: {color}</li>
+        <li>Height: {height}cm</li>
+        <li>Weight: {weight}kg</li>
+        <li>Hypoallergenic: {hypoallergenic ? "Yes" : "No"}</li>
       </ul>
-      {currentPet.bio ? (
-        <article className="bio">{currentPet.bio}</article>
-      ) : null}
+      <article className="bio">{bio}</article>
     </>
   );
 }
--- src/components/Pet/PetContext.js
+++ src/components/Pet/PetContext.js
@@ -17,8 +17,23 @@ export default function PetContextWrapper({ children }) {
     }
   }
 
+  async function savePet(id) {
+    const token = JSON.parse(localStorage.getItem("token"));
+    const headersConfig = { headers: { Authorization: `Bearer ${token}` } };
+    try {
+      const saved = await axios.post(
+        `${process.env.REACT_APP_URL}/pets/:${id}/save`,
+        null,
+        headersConfig
+      );
+      alert("Pet saved successfully");
+    } catch (err) {
+      alert(err.response.data);
+    }
+  }
+
   return (
-    <PetContext.Provider value={{ currentPet, getPetById }}>
+    <PetContext.Provider value={{ currentPet, getPetById, savePet }}>
       {children}
     </PetContext.Provider>
   );
